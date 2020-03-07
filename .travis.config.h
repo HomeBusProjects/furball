71906ddca206e04945396624bf97371cd61c91cf
John Romkey <58883+romkey@users.noreply.github.com>
2020-03-06 20:51:51 -0800

    new config.h file Travis CI builds
    cp config.h file before build

---
+++ .travis.config.h
@@ -0,0 +1,10 @@
+#define WIFI_SSID1     "wifi name 1"
+#define WIFI_PASSWORD1 "wifi password 1"
+
+#define WIFI_SSID2     "wifi name 2"
+#define WIFI_PASSWORD2 "wifi password 2"
+
+#define WIFI_SSID3     "wifi name 3"
+#define WIFI_PASSWORD3 "wifi password 3"
+
+#define UPDATE_DELAY 60*1000
